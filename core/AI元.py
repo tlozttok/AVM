d5f42cbd925fde35a62be0519359907ec9397324
wjzjh <zhaojunhao550@gmail.com>
2025-03-06 22:20:53 +0800

    refactor(core): 重构 AI元类并添加新功能
    - 添加抽象方法和新功能，增强 AI 元类的可扩展性
    - 新增对话轮数统计、系统提示设置和用户提示提供方法
    - 优化信息处理流程，支持多轮对话
    -重构 Context 类，增加消息统计和消息发送功能

--- core/AI元.py
+++ core/AI元.py
@@ -1,7 +1,8 @@
+from abc import abstractmethod
 from typing import List
 
 from core.Information import 信息
-from core.通信层 import Context
+from core.通信层 import Context, Setting
 from 信息.角色提示信息 import 角色提示信息
 
 class 系统元:
@@ -26,6 +27,16 @@ class AI元:
 
     对话结果:List[信息]
 
+    def __init__(self):
+        self._对话内容=Context(messages=[],setting=Setting())
+        self.背景知识=[]
+        self.程序提示=[]
+        self.对话结果=[]
+
+    @property
+    def 对话轮数(self):
+        return self._对话内容.统计AI消息数()
+
 
     def 增加背景知识(self,新信息:信息):
         self.背景知识.append(新信息)
@@ -33,8 +44,36 @@ class AI元:
     def 设置程序提示(self,程序提示:List[信息]):
         self.程序提示=程序提示
 
+    @abstractmethod
+    def _get_system_prompt(self):
+        raise NotImplementedError()
 
+    @abstractmethod
+    def _get_user_prompt(self,对话轮数):
+        raise NotImplementedError()
+
+    def _user_prompt_provider(self):
+        对话轮数=self.对话轮数
+        instruction=self._get_user_prompt(对话轮数)
+        while instruction:
+            yield instruction
+            instruction=self._user_prompt_provider()
+            对话轮数+=1
+
+    @abstractmethod
+    def extract_result(self):
+        raise NotImplementedError()
+
+    def process(self):
+        system_prompt=self._get_system_prompt()
+        self._对话内容.set_system_prompt(system_prompt)
+        for instruction in self._user_prompt_provider():
+            self._对话内容.append_user_prompt(instruction)
+            self._对话内容.send()
 
 
 class 角色化AI元(AI元):
     角色信息:角色提示信息
+
+
+
--- core/通信层.py
+++ core/通信层.py
@@ -31,3 +31,18 @@ class Context:
         self.messages=messages
         self.setting=setting
 
+    def 统计AI消息数(self):
+        return len(list(filter(lambda x:x.role==MessageRole.ASSISTANT,self.messages)))
+
+    def set_system_prompt(self,prompt:str):
+        self.messages.insert(0,Message(MessageRole.SYSTEM,prompt))
+
+    def append_user_prompt(self,prompt:str):
+        self.messages.append(Message(MessageRole.USER,prompt))
+
+    def append_assistant_prompt(self,prompt:str):
+        self.messages.append(Message(MessageRole.ASSISTANT,prompt))
+
+    def send(self):
+        # 发送请求
+        raise NotImplementedError
